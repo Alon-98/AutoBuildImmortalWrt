defed49fedb2f9b830f8740275a1ca90990569c2
Alon-98 <samtning@petalmail.com>
2025-03-08 18:06:43 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -30,19 +30,20 @@ uci set "dhcp.@domain[-1].ip=203.107.6.88"
     count=$((count + 1))
     ifnames="$ifnames $iface_name"
   fi
-done
+#done
 # 删除多余空格
-ifnames=$(echo "$ifnames" | awk '{$1=$1};1')
+#ifnames=$(echo "$ifnames" | awk '{$1=$1};1')
+ ifnames=$(echo "$ifnames" | awk '{$1=$1};1')
 
 # 网络设置
 #if [ "$count" -eq 1 ]; then
-if [ "$count" -eq 1 ]; then
+#if [ "$count" -eq 1 ]; then
    # 单网口设备 类似于NAS模式 动态获取ip模式 具体ip地址取决于上一级路由器给它分配的ip 也方便后续你使用web页面设置旁路由
    # 单网口设备 不支持修改ip 不要在此处修改ip 
    #uci set network.lan.proto='dhcp'
-   uci set network.lan.proto='static'
+#    uci set network.lan.proto='static'
 #elif [ "$count" -gt 1 ]; then
- elif [ "$count" -gt 1 ]; then
+# elif [ "$count" -gt 1 ]; then
    # 提取第一个接口作为WAN
    #wan_ifname=$(echo "$ifnames" | awk '{print $1}')
    #提取第一个接口为LAN
@@ -78,7 +79,7 @@ if [ "$count" -eq 1 ]; then
    uci set network.lan.ipaddr='10.10.10.220'
    uci set network.lan.netmask='255.255.255.0'
    uci set dhcp.lan.ignore='1'
-   #echo "set 10.10.10.220 at $(date)" >> $LOGFILE
+   echo "set 10.10.10.220 at $(date)" >> $LOGFILE
    # 判断是否启用 PPPoE
    #echo "print enable_pppoe value=== $enable_pppoe" >> $LOGFILE
    #if [ "$enable_pppoe" = "yes" ]; then
@@ -90,12 +91,12 @@ if [ "$count" -eq 1 ]; then
       #uci set network.wan.peerdns='1'
       #uci set network.wan.auto='1'
       # 设置ipv6 默认不配置协议
-      uci set network.wan6.proto='none'
+      #uci set network.wan6.proto='none'
       #echo "PPPoE configuration completed successfully." >> $LOGFILE
    #else
       #echo "PPPoE is not enabled. Skipping configuration." >> $LOGFILE
    #fi
-   fi
+   #fi
 fi
 
 
